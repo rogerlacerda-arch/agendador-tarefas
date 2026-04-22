1e794f18e9bfeb3e4832eb58fb7df496299d7b60
Roger Lacerda <lacerdarooh@gmail.com>
2026-04-22 13:37:16 -0300

    Commit inicial com esqueleto

---
+++ src/test/java/com/rogerLacerda/agendador_tarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerLacerda.agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
